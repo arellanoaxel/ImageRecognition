23d9c641630c6000372464836597254eef348e31
bwb1372 <bwb1372@vt.edu>
2024-03-13 16:30:20 -0400

    added saving samples matrix and categories vector as npz and created initial KMean

--- KMeans.py
+++ KMeans.py
@@ -1,24 +1,148 @@
 import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import scale
+import matplotlib.pyplot as plt
+from collections import Counter
+from sklearn.decomposition import PCA
 
-grey_scale_arrays_npz_file = "./projectData/grey_scale_arrays.npz"
+grey_scale_samples_and_category_ids_npz_file = "./projectData/grey_scale_samples_and_category_ids.npz"
 
-# load npz into dic
-grey_scale_arrays_npz_dic = np.load(grey_scale_arrays_npz_file)
+grey_scale_samples_and_category_ids_dic = np.load(grey_scale_samples_and_category_ids_npz_file)
 
-# get list of array names (ids)
-grey_scale_array_ids = grey_scale_arrays_npz_dic.files
+X = grey_scale_samples_and_category_ids_dic['grey_scale_samples']
+X = scale(X)
+y = grey_scale_samples_and_category_ids_dic['category_ids']
 
-i = 1
-for id in grey_scale_array_ids:
-    # get an array based on name/id
-    grey_scale_array = grey_scale_arrays_npz_dic[id]
+
+
+bird = 0 # id 0
+coyote = 20 # id 20
+bear = 22 # id 22
+
+
+wanted = [bear, bird]
+
+# make new sample matrix and category array (X,y) but only include
+# the wanted categories
+def make_sample_matrix_and_category_array(X,y,*wanted_categories):
+    truth_index = []
+    for category in wanted_categories:
+        truth_index.append((y == category))
     
-    
-    # just showing that these are correct for first 4 images
-    print(id) # printing id
-    print(grey_scale_array.shape) # showing shape is correct (64,64)
-    print(grey_scale_array, end = '\n\n') # print the gray scale array
-    if i == 4:
-        break
-    i += 1
+    wanted_indexes = np.where(np.logical_or.reduce(truth_index))[0]
+    wanted_y = y[wanted_indexes]
+    wanted_X = X[wanted_indexes]
+    return wanted_X, wanted_y
+
+new_X, new_y = make_sample_matrix_and_category_array(X,y,*wanted)
+
+# perform KMeans
+n_catagories = 2
+kmeans = KMeans(n_clusters=n_catagories, random_state=12, n_init=10)
+predictions = kmeans.fit_predict(new_X)
+
+color_themes = np.array(['black',
+                         'red',
+                         'orange',
+                         'green',
+                         'blue',
+                         'm',
+                         'lightpink',
+                         'violet',
+                         'cornflowerblue',
+                         'lime',
+                         'aquamarine',
+                         'darkcyan',
+                         'lightskyblue',
+                         'palegreen',
+                         'sienna',
+                         'peachpuff',
+                         'lightgrey',
+                         'yellow',
+                         'gold',
+                         'c',
+                         'thistle',
+                         'palevioletred',
+                         'darkgoldenrod'])
+
+# use two features to plot and color their points with label/category color
+feature_0 = new_X[:,0]
+feature_1 = new_X[:,1]
+fig, axs = plt.subplots(2)
+
+# plot these so you can visuallly see how to reorder
+# axs[0].scatter(x=feature_0, y=feature_1, c = color_themes[new_y])
+# axs[1].scatter(x=feature_0, y=feature_1, c = color_themes[predictions])
+
+
+# need to reorder cluster labels before we can calculate accuracy
+predictions[predictions == 0] = 0
+# predictions[predictions == 2] = 0
+predictions[predictions == 1] = 22
+
+
+plt.subplot(2,1,1)
+# plot known labels/truth
+plt.scatter(x=feature_0, y=feature_1, c = color_themes[new_y])
+plt.title('Truth (Bears & birds)')
+
+plt.subplot(2,1,2)
+# plot predictions
+plt.scatter(x=feature_0, y=feature_1, c = color_themes[predictions])
+plt.title('Predictions')
+print(f'Accuracy = {accuracy_score(new_y, predictions)}')
+plt.show()
+
+# use PCA to reduce to 2D and plot after fitting
+reduced_data = PCA(n_components=2).fit_transform(new_X)
+kmeans = KMeans(n_clusters=n_catagories, n_init=10)
+kmeans.fit(reduced_data)
+
+# Step size of the mesh
+h = 0.02  
+
+# decision boundary
+x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
+y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
+
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+# labels for each point in mesh
+Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# color plot
+Z = Z.reshape(xx.shape)
+plt.figure(1)
+plt.clf()
+plt.imshow(
+    Z,
+    interpolation="nearest",
+    extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+    cmap=plt.cm.Paired,
+    aspect="auto",
+    origin="lower",
+)
+
+plt.plot(reduced_data[:, 0], reduced_data[:, 1], "k.", markersize=2)
+# Plot the centroids 
+centroids = kmeans.cluster_centers_
+plt.scatter(
+    centroids[:, 0],
+    centroids[:, 1],
+    marker="x",
+    s=169,
+    linewidths=3,
+    color="w",
+    zorder=10,
+)
+plt.title(
+    "K-means clustering on bears & birds dataset (PCA-reduced data)\n"
+    "Centroids are marked with white X"
+)
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
     
--- processImages.py
+++ processImages.py
@@ -6,7 +6,7 @@ import numpy as np
 def extract_and_preprocess_roi(image_path, annotations, output_dir):
     # Read the image
     image = cv2.imread(image_path)
-    annotation_id, resized_roi = None, None
+    annotation_id, resized_roi, category_id = None, None, None
     
     # Iterate through annotations
     for annotation in annotations:
@@ -26,17 +26,20 @@ def extract_and_preprocess_roi(image_path, annotations, output_dir):
             
             # Save the preprocessed ROI
             annotation_id = annotation['id']
+            category_id = annotation['category_id']
+            
             roi_filename = f"{annotation_id}.jpg"
             roi_path = os.path.join(output_dir, roi_filename)
             cv2.imwrite(roi_path, resized_roi)
             print(f"Saved preprocessed ROI: {roi_filename}")
             
-    return annotation_id, resized_roi
+    return annotation_id, resized_roi, category_id
 
 # Set paths
 input_dir =  "./projectData/ena24"  # path to input images directory
 json_file = "./projectData/metadata.json" # path to json
 grey_scale_arrays_npz_file = "./projectData/grey_scale_arrays.npz"
+grey_scale_samples_and_category_ids_npz_file = "./projectData/grey_scale_samples_and_category_ids.npz"
 output_dir = "./projectData/processed"  # path to output directory
 
 # Load JSON file containing metadata
@@ -48,13 +51,43 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 grey_scale_arrays_and_ids = {}
+grey_scale_samples = []
+category_ids = []
 
-# Iterate through images and extract & preprocess ROIs
+# Iterate through images and extract preprocess ROIs, annotation ids and category ids
 for image_info in metadata['images']:
     image_id = image_info['id']
     image_path = os.path.join(input_dir, image_info['file_name'])
     annotations = [ann for ann in metadata['annotations'] if ann['image_id'] == image_id]
-    annotation_id, resized_roi = extract_and_preprocess_roi(image_path, annotations, output_dir)
+    annotation_id, resized_roi, category_id = extract_and_preprocess_roi(image_path, annotations, output_dir)
+    
+    # store roi with annotation_id key
     grey_scale_arrays_and_ids[annotation_id] = resized_roi
+    
+    # flatten roi matrix into one row of grey scale features
+    grey_scale_sample_features = resized_roi.flatten()
+    
+    # store each row of grey scale features
+    grey_scale_samples.append(grey_scale_sample_features)
+    
+    # store corresponding category_id
+    category_ids.append(category_id)
 
+
+# save npz file of grey scale matrixes of each image stored as np.arrays
 np.savez(grey_scale_arrays_npz_file, **grey_scale_arrays_and_ids)
+
+# X: grey scale samples matrix (np.array) where each row is an image
+# and that row's columns are the grey scale feature values
+X = np.vstack(grey_scale_samples)
+
+# y: np.array of the corresponding categories
+y = np.array(category_ids).flatten() 
+ 
+# np.arrays to save: sample matrix and category array to save
+samples_and_categories_arrays = {'grey_scale_samples' : X,'category_ids' : y}
+
+# save npz file of grey scale samples matrix and corresponding category vector, matrix X and vector y
+# these are stored as np.arrays and can be easily accessed through the names 'grey_scale_samples' and
+# 'category_ids' after loading, np.load(), the npz file
+np.savez(grey_scale_samples_and_category_ids_npz_file, **samples_and_categories_arrays)
